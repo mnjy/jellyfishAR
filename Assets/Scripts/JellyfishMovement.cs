3afd91c8d3d3ca0ce749550a8cb67d344c02e8a4
Misha <mnjy@mit.edu>
2017-04-24 05:46:52 -0400

    Jellyfish now move away from touch position after 10s. Need to tune speed and movement radius and sizes etc. based on actual mural.

--- Assets/Scripts/JellyfishMovement.cs
+++ Assets/Scripts/JellyfishMovement.cs
@@ -12,12 +12,15 @@ public class JellyfishMovement : MonoBehaviour {
     float currTime;
     float startDelay;
 
-    //For moving to finger
-    public Vector3 destination;
+    //For moving to/from finger
+    Vector3 destination = Vector3.zero;
     float depth = 2f;
     public float moveSpeed;
     float distanceThreshold = 1f;
-
+    bool movingAwayFromFinger = false;
+    public float maxTimeToSpendAtFingerPos = 2f;
+    float timeSpentAtFingerPos = 0;
+    
     void Start () {
         basePosition = transform.position;
         goingUp = true;
@@ -29,7 +32,7 @@ public class JellyfishMovement : MonoBehaviour {
 	
 	void Update () {
         Bob();
-        MoveToFinger();
+        MoveToOrFromFingerPos();
     }
 
     void Bob()
@@ -49,21 +52,34 @@ public class JellyfishMovement : MonoBehaviour {
         if (displacement >= maxDisplacement) goingUp = !goingUp;
     }
 
-    void MoveToFinger()
+    void MoveToOrFromFingerPos()
     {
         //check if user touched screen
         if (Input.touchCount > 0)
         {
             destination = Input.GetTouch(0).position;
             destination.z = depth;
+            timeSpentAtFingerPos = 0;
         }
 
-        if (destination != null && Vector3.Distance(destination, basePosition) > distanceThreshold)
+        if (destination == Vector3.zero)
+        {
+            return; //user has not touched screen yet, no action
+        } else if (Vector3.Distance(destination, basePosition) > distanceThreshold)
         {
             Vector3 movementDirection = (destination - basePosition).normalized;
-            print(movementDirection);
             transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
             basePosition += movementDirection * moveSpeed * Time.deltaTime;
+        } else
+        {
+            timeSpentAtFingerPos += Time.deltaTime;
+            if (timeSpentAtFingerPos > maxTimeToSpendAtFingerPos)
+            {
+                //pick a random new destination
+                destination = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), transform.position.z); //stay in the same z plane
+                //reset timer
+                timeSpentAtFingerPos = 0;
+            }
         }
     }
 }
