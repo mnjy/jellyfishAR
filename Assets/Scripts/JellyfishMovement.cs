f55f7935d82b67c951ba2d6edc8bd1e255f61ddb
Misha <mnjy@mit.edu>
2017-04-24 01:19:10 -0400

    Added code to move to finger but buggy atm

---
+++ Assets/Scripts/JellyfishMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JellyfishMovement : MonoBehaviour {
+
+    //For bobbing
+    public float maxDisplacement;
+    public float bobSpeed;
+    Vector3 basePosition; //position it should return to
+    bool goingUp;
+
+    float currTime;
+    float startDelay;
+
+    //For moving to finger
+    public Vector3 destination;
+    float depth = 2f;
+    public float moveSpeed;
+    float distanceThreshold = 1f;
+
+    void Start () {
+        basePosition = transform.position;
+        goingUp = true;
+
+        //add a random delay so that they don't all start at the same time
+        currTime = 0;
+        startDelay = Random.Range(0, 2f);
+	}
+	
+	void Update () {
+        Bob();
+        MoveToFinger();
+    }
+
+    void Bob()
+    {
+        if (currTime < startDelay)
+        {
+            currTime += Time.deltaTime;
+            return;
+        }
+
+        //update the position
+        if (goingUp) transform.Translate(transform.up * bobSpeed);
+        else transform.Translate(transform.up * bobSpeed * -1);
+
+        //check if should switch direction
+        float displacement = Vector3.Distance(transform.position, basePosition);
+        if (displacement >= maxDisplacement) goingUp = !goingUp;
+    }
+
+    void MoveToFinger()
+    {
+        //check if user touched screen
+        if (Input.touchCount > 0)
+        {
+            destination = Input.GetTouch(0).position;
+            destination.z = depth;
+        }
+
+        if (destination != null && Vector3.Distance(destination, basePosition) > distanceThreshold)
+        {
+            Vector3 movementDirection = (destination - basePosition).normalized;
+            print(movementDirection);
+            transform.Translate(movementDirection * moveSpeed);
+            basePosition += movementDirection * moveSpeed;
+        }
+    }
+}
